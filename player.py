4fb35feca43adae3a42c2da851607b66af1fe27a
Tymon <tymon.jankowski@bsr.edu>
2021-04-29 09:06:28 +0200

    Animation for movement implemented

--- player.py
+++ player.py
@@ -1,36 +1,63 @@
 import pygame
 
+sprites = [ pygame.image.load('images/bomber1.bmp'), pygame.image.load('images/bomber2.bmp') ]
+
 class Player():
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
-        self.image = pygame.image.load('images/ship.bmp')
+        self.move_phase = 0
+        self.image = sprites[self.move_phase]
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
-        
 
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
         
+        
+        
+    def advance_image(self):
+        if self.move_phase > 5:
+            self.image = sprites[1]
+        else:
+            self.image = sprites[0]
+            
+    def animate(self):
+        self.move_phase += 1
+        
+        if self.move_phase > 10:
+            self.move_phase = 0
+        
+        self.advance_image()
+        
     def update(self):
+
+            
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
+            self.animate()
+            
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+            self.animate()
+            
         if self.moving_up and self.y > 0:
             self.y -= self.settings.ship_speed
+            self.animate()
+            
         if self.moving_down:
             self.y += self.settings.ship_speed
-
+            self.animate()
+            
         self.rect.x = self.x
         self.rect.y = self.y
     def blitme(self):
-        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+        
